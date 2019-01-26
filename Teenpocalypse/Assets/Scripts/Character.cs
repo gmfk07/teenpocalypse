33c639f92e47556ce002bca73a39e7f6a3873405
Andrew Hoyt <andrew@chillhammer.com>
2019-01-25 23:01:52 -0500

    Constants, Game Controller, Character Scripts
    Setup

---
+++ Teenpocalypse/Assets/Scripts/Character.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+	public string Name = "Bill";
+	[Range(0, 100)] public int Health;
+	[Range(0, 100)] public int Hunger;
+
+
+
+	void Start()
+	{
+
+	}
+	void Update()
+	{
+
+	}
+}
