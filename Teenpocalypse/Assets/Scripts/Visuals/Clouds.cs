9421dcef4227d18ed0565faf78aff785c8f06022
Ali Rahman <amrahman@mit.edu>
2019-01-27 03:34:43 -0500

    Made DayNightFilter a slider in a script, and some other minor visual effects stuff

---
+++ Teenpocalypse/Assets/Scripts/Visuals/Clouds.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Clouds : MonoBehaviour {
+    public GameObject clouds1;
+    public GameObject clouds2;
+    public float startX;
+    public float midX;
+    public float endX;
+    public float speed;
+    private bool _one = true;
+    private bool _two = false;
+
+    void Update() {
+        if(_one)
+            clouds1.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if(_two)
+            clouds2.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if(_one) {
+            if(clouds1.transform.localPosition.x > midX)
+                _two = true;
+            if(clouds1.transform.localPosition.x > endX) {
+                _one = false;
+                clouds1.transform.localPosition = new Vector3(startX, 0, 0);
+            }
+        }
+        if(_two) {
+            if(clouds2.transform.localPosition.x > midX)
+                _one = true;
+            if(clouds2.transform.localPosition.x > endX) {
+                _two = false;
+                clouds2.transform.localPosition = new Vector3(startX, 0, 0);
+            }
+        }
+    }
+}
--- Teenpocalypse/Assets/Scripts/Visuals/DayNightFilter.cs
+++ Teenpocalypse/Assets/Scripts/Visuals/DayNightFilter.cs
@@ -1,23 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
+[ExecuteInEditMode]
 public class DayNightFilter : MonoBehaviour {
+    [Range(0, 1)] [Tooltip("How night is it")]
+    public float nightness;
+
+    public List<GameObject> shadowsToFadeOut;
     private PostProcessVolume _volume;
-    private Vignette _vignette;
+    private Light _light;
+    private float _oldNightness;
 
     private void Start() {
-        _vignette = ScriptableObject.CreateInstance<Vignette>();
-        _vignette.enabled.Override(true);
-        _vignette.intensity.Override(1f);
-
-        _volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, _vignette);
+        _volume = GetComponent<PostProcessVolume>();
+        _light = GetComponent<Light>();
+        _oldNightness = nightness;
     }
 
     private void Update() {
-        _vignette.intensity.value = Mathf.Sin(Time.realtimeSinceStartup);
-    }
-
-    private void OnDestroy() {
-        RuntimeUtilities.DestroyVolume(_volume, true, true);
+        _volume.weight = nightness;
+        _light.intensity = Mathf.Lerp(1, 0.25f, nightness);
+        if(Math.Abs(_oldNightness - nightness) > 0.01f) {
+            foreach(var shadow in shadowsToFadeOut) {
+                shadow.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, Color.clear, nightness);
+            }
+        }
+        _oldNightness = nightness;
     }
 }
