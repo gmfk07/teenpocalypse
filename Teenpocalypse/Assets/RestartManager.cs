e700e0bea06efc08a1389c84de7dd6938a7d9411
Greg Ludovici <greg.ludovici@gmail.com>
2019-01-27 04:07:15 -0500

    Revised Game Over to hide new Building Controllers and enabled Play Again button

---
+++ Teenpocalypse/Assets/RestartManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartManager : MonoBehaviour
+{ 
+    public void RestartGameController()
+    {
+        Destroy(GameObject.Find("GameController"));
+    }
+}
