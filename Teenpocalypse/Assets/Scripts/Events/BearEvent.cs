7df4906f79d731f66ec079333435ac27b4d42249
Julian Hernandez <gmfk07@gmail.com>
2019-01-27 01:51:11 -0500

    Update Bear

--- Teenpocalypse/Assets/Scripts/Events/BearEvent.cs
+++ Teenpocalypse/Assets/Scripts/Events/BearEvent.cs
@@ -16,9 +16,20 @@ public class BearEvent : Event
         {
             case 0:
                 if (gc.TestDefense(1))
+                {
                     success = true;
+
+                }
                 else
+                {
                     success = false;
+                    loss = gc.Food - (gc.Food / 2);
+                    gc.Food = gc.Food / 2;
+                    if (gc.OnDefense.Count > 0)
+                        target = gc.OnDefense[Random.Range(0, gc.OnDefense.Count)];
+                    else
+                        target = gc.Roster[Random.Range(0, gc.Roster.Count)];
+                }
                 break;
 
             case 1:
@@ -34,13 +45,12 @@ public class BearEvent : Event
 		{
 			case 0:
                 if (success)
-				    return "The bear was driven away! Three cheers for our brave fighters!\nMorale: +10";
+                    return "The bear was driven away! Three cheers for our brave fighters!\nMorale: +10";
                 else
-                    return "We failed in our defense. The bear got into our food supply, and"
-                break;
+                    return "We failed in our defense. The bear got into our food supply, and " + target.Name + " was hurt.\nFood: -" + loss;
 
 			case 1:
-				return "The bear is given a wide berth, as they start to eat the food supply. Not much is left.\nFood: \nMorale: -5";
+				return "The bear is given a wide berth, as they start to eat the food supply. Not much is left.\nFood: -" + loss + "\nMorale: -5";
 		}
 		return "";
 	}
