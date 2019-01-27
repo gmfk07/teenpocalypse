29239d968a7a58983f2ea082ac98778f4bddc0f8
Andrew Hoyt <andrew@chillhammer.com>
2019-01-27 00:46:42 -0500

    Events 2.0

--- Teenpocalypse/Assets/Scripts/Events/Event.cs
+++ Teenpocalypse/Assets/Scripts/Events/Event.cs
@@ -9,7 +9,17 @@ public abstract class Event : ScriptableObject
     [Range(0, 10)] public int MinWeek;
     public List<string> Choices;
 	public bool HasConsequencesText = true;
+	public bool UseDynamicDescription = false;
 
     public abstract void Execute(int choice);
+	public virtual string GetDescription()
+	{
+		return Description;
+	}
+	public virtual List<string> GetChoices()
+	{
+		return Choices;
+	}
+	public virtual void Chosen() { };
 	public abstract string GetConsequencesText(int choice);
 }
